e7ba710d408f6df2151f6e45b7a570cf4c2183dd
Darion Richards <darionrichards@outlook.com>
2022-02-18 13:54:07 +0000

    Imported GroupProject Component into App
    styles.css
    
    - added styles to wrapper

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import AboutMe from "./components/AboutMe";
+import {GroupProjects} from "./components/GroupProjects";
+// import {IndividualProjects} from "./components/IndividualProjects";
 import Nav from "./components/Nav";
 import "./reset.css";
 import "./styles.css";
@@ -11,6 +13,8 @@ function App() {
 			</header>
 			<main>
 				<AboutMe />
+				<GroupProjects />
+				{/* <IndividualProjects /> */}
 			</main>
 		</>
 	);
