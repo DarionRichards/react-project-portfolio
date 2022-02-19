eb46bbb912c629b0ad5f70a8f929d22d7d187b06
Darion Richards <darionrichards@outlook.com>
2022-02-19 14:31:37 +0000

    Created Individual Projects Section and render projects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import AboutMe from "./components/AboutMe";
 import {GroupProjects} from "./components/GroupProjects";
-// import {IndividualProjects} from "./components/IndividualProjects";
+import {IndividualProjectsSection} from "./components/IndividualProjectsSection";
 import Nav from "./components/Nav";
 import "./reset.css";
 import "./styles.css";
+
+// const projectsArray = [
+// 	{
+// 		id: 1,
+// 		title: "Coding Quiz Challenge",
+// 		text: "A dynamic web application where users answer a series of questions and stores their score after the questions have been answered. After the user has finished with the quiz, all their scores will be stored.",
+// 		deployedLink: "https://darionrichards.github.io/quiz-challenge/",
+// 		gitHubRepo: "https://github.com/DarionRichards/quiz-challenge",
+// 	},
+// ];
 
 function App() {
 	return (
@@ -14,7 +24,7 @@ function App() {
 			<main>
 				<AboutMe />
 				<GroupProjects />
-				{/* <IndividualProjects /> */}
+				<IndividualProjectsSection />
 			</main>
 		</>
 	);
---
+++ src/components/IndividualProjectCard/index.js
@@ -0,0 +1,60 @@
+import "./IndividualProjectCard.css";
+
+export const IndividualProjectCard = (props) => {
+	return (
+		<section className="project-card">
+			{props.deployedLink ? (
+				<a
+					href={props.deployedLink}
+					style={
+						({backgroundColor: "gray"},
+						{borderRadiusInputTopLeft: "1rem"},
+						{borderRadiusInputTopRight: "1rem"})
+					}
+				>
+					<img
+						className="project-img"
+						src={props.imagePath}
+						alt={props.imageAlt}
+						width="250"
+						height="350"
+					/>
+				</a>
+			) : (
+				<a
+					href={props.gitHubRepo}
+					style={
+						({backgroundColor: "gray"},
+						{borderRadiusInputTopLeft: "1rem"},
+						{borderRadiusInputTopRight: "1rem"})
+					}
+				>
+					<img
+						className="project-img"
+						src={props.imagePath}
+						alt={props.imageAlt}
+						width="250"
+						height="350"
+					/>
+				</a>
+			)}
+
+			<div className="project-card-body">
+				<div>
+					<h3 className="project-card-title">{props.title}</h3>
+					<p className="project-card-text">{props.text}</p>
+				</div>
+				<div className="card-button-container">
+					{props.deployedLink && (
+						<a href={props.deployedLink} className="button">
+							Deployed App
+						</a>
+					)}
+					<a href={props.gitHubRepo} className="button">
+						GitHub Repository
+					</a>
+				</div>
+			</div>
+		</section>
+	);
+};
