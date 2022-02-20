434259918001cd63c34d0986ee168b6882fab15f
Darion Richards <darionrichards@outlook.com>
2022-02-20 12:05:51 +0000

    Removed comments from App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,6 @@ import Nav from "./components/Nav";
 import "./reset.css";
 import "./styles.css";
 
-// const projectsArray = [
-// 	{
-// 		id: 1,
-// 		title: "Coding Quiz Challenge",
-// 		text: "A dynamic web application where users answer a series of questions and stores their score after the questions have been answered. After the user has finished with the quiz, all their scores will be stored.",
-// 		deployedLink: "https://darionrichards.github.io/quiz-challenge/",
-// 		gitHubRepo: "https://github.com/DarionRichards/quiz-challenge",
-// 	},
-// ];
-
 function App() {
 	return (
 		<>
