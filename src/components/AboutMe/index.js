921e3dd1fa4e5dc65b9bad0e79bb100953a12ea2
Darion Richards <darionrichards@outlook.com>
2022-02-20 12:27:06 +0000

    Added animation and tilt effects to AboutMe Section
    - included media queries for different view widths

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,3 +1,5 @@
+import {AnimationOnScroll} from "react-animation-on-scroll";
+import Tilt from "react-parallax-tilt";
 import "./AboutMe.css";
 
 const AboutMe = () => {
@@ -5,56 +7,124 @@ const AboutMe = () => {
 		<>
 			<section id="about-me" className="about-me-section wrapper">
 				<div>
-					<div className="heading">
-						<h1>ABOUT ME</h1>
-					</div>
-					<p className="about-text">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil natus
-						recusandae amet nostrum odit, ut unde non sint, quia sed inventore
-						odio, delectus iure! Eligendi vitae fuga blanditiis accusamus
-						tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Nihil natus recusandae amet nostrum odit, ut unde non sint, quia sed
-						inventore odio, delectus iure! Eligendi vitae fuga blanditiis
-						accusamus tempore! Lorem ipsum dolor sit amet consectetur
-						adipisicing elit. Nihil natus recusandae amet nostrum odit, ut unde
-						non sint, quia sed inventore odio, delectus iure! Eligendi vitae
-						fuga blanditiis accusamus tempore!
-					</p>
+					<AnimationOnScroll
+						animateOnce={true}
+						animateIn="animate__lightSpeedInLeft"
+						duration={1.8}
+					>
+						<div className="heading">
+							<h1>ABOUT ME</h1>
+						</div>
+						<AnimationOnScroll
+							animateOnce={true}
+							animateIn="animate__lightSpeedInLeft"
+							duration={1.8}
+							delay={1250}
+						>
+							<p className="about-text">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+								natus recusandae amet nostrum odit, ut unde non sint, quia sed
+								inventore odio, delectus iure! Eligendi vitae fuga blanditiis
+								accusamus tempore! Lorem ipsum dolor sit amet consectetur
+								adipisicing elit. Nihil natus recusandae amet nostrum odit, ut
+								unde non sint, quia sed inventore odio, delectus iure! Eligendi
+								vitae fuga blanditiis accusamus tempore! Lorem ipsum dolor sit
+								amet consectetur adipisicing elit. Nihil natus recusandae amet
+								nostrum odit, ut unde non sint, quia sed inventore odio,
+								delectus iure! Eligendi vitae fuga blanditiis accusamus tempore!
+							</p>
+						</AnimationOnScroll>
+						<AnimationOnScroll
+							animateOnce={true}
+							animateIn="animate__lightSpeedInLeft"
+							duration={1.8}
+							delay={2250}
+						>
+							<p className="about-text">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+								natus recusandae amet nostrum odit, ut unde non sint, quia sed
+								inventore odio, delectus iure! Eligendi vitae fuga blanditiis
+								accusamus tempore! Lorem ipsum dolor sit amet consectetur
+								adipisicing elit. Nihil natus recusandae amet nostrum odit, ut
+								unde non sint, quia sed inventore odio, delectus iure! Eligendi
+								vitae fuga blanditiis accusamus tempore! Lorem ipsum dolor sit
+								amet consectetur adipisicing elit. Nihil natus recusandae amet
+								nostrum odit, ut unde non sint, quia sed inventore odio,
+								delectus iure! Eligendi vitae fuga blanditiis accusamus tempore!
+							</p>
+						</AnimationOnScroll>
+					</AnimationOnScroll>
 				</div>
-
-				<section className="profile-card">
-					<div className="centre">
-						<img
-							className="profile-img"
-							src="./assets/images/profile-img.png"
-							alt="Projects Author (Darion Richards)"
-						/>
-						<h2>Darion Richards</h2>
-						<div className="profile-status">
-							<i className="fas fa-briefcase"></i>
-							<h6>Full Stack Developer</h6>
-						</div>
-						<div className="profile-location">
-							<i className="fas fa-map-marker-alt"></i>
-							<h6>Birmingham, UK</h6>
-						</div>
-					</div>
-					<div className="profile-card-skills centre">
-						<i className="fab fa-html5 gradient"></i>
-						<i className="fab fa-css3-alt"></i>
-						<i className="fab fa-js"></i>
-						<i className="fab fa-node"></i>
-						<i className="fab fa-react"></i>
-					</div>
-					<div className="profile-card-links">
-						<a href="https://github.com/DarionRichards">
-							<i className="fab fa-github"></i>
-						</a>
-						<a href="https://www.linkedin.com/in/darionrichards/">
-							<i className="fab fa-linkedin"></i>
-						</a>
-					</div>
-				</section>
+				<AnimationOnScroll
+					animateOnce={true}
+					animateIn="animate__slideInRight"
+					duration={2}
+				>
+					<Tilt className="tilt-element" perspective={700} scale={1.02}>
+						<section className="profile-card inner-tilt-element">
+							<div className="centre">
+								<img
+									className="profile-img"
+									src="./assets/images/profile-img.png"
+									alt="Projects Author (Darion Richards)"
+								/>
+								<h2>Darion Richards</h2>
+								<div className="profile-status">
+									<i className="fas fa-briefcase"></i>
+									<h6>Full Stack Developer</h6>
+								</div>
+								<div className="profile-location">
+									<i className="fas fa-map-marker-alt"></i>
+									<h6>Birmingham, UK</h6>
+								</div>
+							</div>
+							<div className="profile-card-skills centre">
+								<AnimationOnScroll
+									animateOnce={true}
+									animateIn="animate__bounceInRight"
+								>
+									<i className="fab fa-html5 gradient"></i>
+								</AnimationOnScroll>
+								<AnimationOnScroll
+									animateOnce={true}
+									animateIn="animate__bounceInRight"
+									delay={1000}
+								>
+									<i className="fab fa-css3-alt"></i>
+								</AnimationOnScroll>
+								<AnimationOnScroll
+									animateOnce={true}
+									animateIn="animate__bounceInRight"
+									delay={1750}
+								>
+									<i className="fab fa-js"></i>
+								</AnimationOnScroll>
+								<AnimationOnScroll
+									animateOnce={true}
+									animateIn="animate__bounceInRight"
+									delay={2500}
+								>
+									<i className="fab fa-node"></i>
+								</AnimationOnScroll>
+								<AnimationOnScroll
+									animateOnce={true}
+									animateIn="animate__bounceInRight"
+									delay={3250}
+								>
+									<i className="fab fa-react"></i>
+								</AnimationOnScroll>
+							</div>
+							<div className="profile-card-links">
+								<a href="https://github.com/DarionRichards">
+									<i className="fab fa-github"></i>
+								</a>
+								<a href="https://www.linkedin.com/in/darionrichards/">
+									<i className="fab fa-linkedin"></i>
+								</a>
+							</div>
+						</section>
+					</Tilt>
+				</AnimationOnScroll>
 			</section>
 		</>
 	);
