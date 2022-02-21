5b59f9524256cfef4141985d62b80ea70333a324
Darion Richards <darionrichards@outlook.com>
2022-02-21 10:25:47 +0000

    Created Individual Projects Section
    - added fadeIn animation to cards when users scroll to section

--- src/components/IndividualProjectCard/index.js
+++ src/components/IndividualProjectCard/index.js
@@ -1,60 +1,68 @@
+import {AnimationOnScroll} from "react-animation-on-scroll";
 import "./IndividualProjectCard.css";
+import "animate.css";
 
 export const IndividualProjectCard = (props) => {
 	return (
-		<section className="project-card">
-			{props.deployedLink ? (
-				<a
-					href={props.deployedLink}
-					style={
-						({backgroundColor: "gray"},
-						{borderRadiusInputTopLeft: "1rem"},
-						{borderRadiusInputTopRight: "1rem"})
-					}
-				>
-					<img
-						className="project-img"
-						src={props.imagePath}
-						alt={props.imageAlt}
-						width="250"
-						height="350"
-					/>
-				</a>
-			) : (
-				<a
-					href={props.gitHubRepo}
-					style={
-						({backgroundColor: "gray"},
-						{borderRadiusInputTopLeft: "1rem"},
-						{borderRadiusInputTopRight: "1rem"})
-					}
-				>
-					<img
-						className="project-img"
-						src={props.imagePath}
-						alt={props.imageAlt}
-						width="250"
-						height="350"
-					/>
-				</a>
-			)}
-
-			<div className="project-card-body">
-				<div>
-					<h3 className="project-card-title">{props.title}</h3>
-					<p className="project-card-text">{props.text}</p>
-				</div>
-				<div className="card-button-container">
-					{props.deployedLink && (
-						<a href={props.deployedLink} className="button">
-							Deployed App
-						</a>
-					)}
-					<a href={props.gitHubRepo} className="button">
-						GitHub Repository
+		<AnimationOnScroll
+			delay={200}
+			animateIn="animate__fadeInLeftBig"
+			animateOnce={true}
+		>
+			<section className="project-card ">
+				{props.deployedLink ? (
+					<a
+						href={props.deployedLink}
+						style={
+							({backgroundColor: "gray"},
+							{borderRadiusInputTopLeft: "1rem"},
+							{borderRadiusInputTopRight: "1rem"})
+						}
+					>
+						<img
+							className="project-img"
+							src={props.imagePath}
+							alt={props.imageAlt}
+							width="250"
+							height="350"
+						/>
 					</a>
+				) : (
+					<a
+						href={props.gitHubRepo}
+						style={
+							({backgroundColor: "gray"},
+							{borderRadiusInputTopLeft: "1rem"},
+							{borderRadiusInputTopRight: "1rem"})
+						}
+					>
+						<img
+							className="project-img"
+							src={props.imagePath}
+							alt={props.imageAlt}
+							width="250"
+							height="350"
+						/>
+					</a>
+				)}
+
+				<div className="project-card-body">
+					<div>
+						<h3 className="project-card-title">{props.title}</h3>
+						<p className="project-card-text">{props.text}</p>
+					</div>
+					<div className="card-button-container">
+						{props.deployedLink && (
+							<a href={props.deployedLink} className="button">
+								Deployed App
+							</a>
+						)}
+						<a href={props.gitHubRepo} className="button">
+							GitHub Repository
+						</a>
+					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+		</AnimationOnScroll>
 	);
 };
