7adee36f615d180db490466be51dc45bd164c750
Darion Richards <darionrichards@outlook.com>
2022-02-24 14:56:17 +0000

    Organised all projectData into a data folder

---
+++ src/data/groupProjectData.js
@@ -0,0 +1,14 @@
+export const groupProjectData = [
+	{
+		imagePath: "./assets/images/readers-revival-screenshot.png",
+		imageAlt: "Readers Revival Example",
+		deployedLink: "https://darionrichards.github.io/readers-revival/",
+		gitHubRepo: "https://github.com/DarionRichards/readers-revival/tree/main",
+	},
+	{
+		imagePath: "./assets/images/fitness-app.png",
+		imageAlt: "Fitness App Example",
+		deployedLink: "",
+		gitHubRepo: "https://github.com/DarionRichards/fitness-app",
+	},
+];
