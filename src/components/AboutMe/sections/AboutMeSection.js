ed6c3c35ec97fc256e7c6ce06469887e893f4e81
Darion Richards <darionrichards@outlook.com>
2022-04-27 11:26:59 +0100

    Added more detail to about me section

--- src/components/AboutMe/sections/AboutMeSection.js
+++ src/components/AboutMe/sections/AboutMeSection.js
@@ -17,16 +17,18 @@ export const AboutMeSection = () => {
 					duration={1.8}
 				>
 					<p className="about-text">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil natus
-						recusandae amet nostrum odit, ut unde non sint, quia sed inventore
-						odio, delectus iure! Eligendi vitae fuga blanditiis accusamus
-						tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Nihil natus recusandae amet nostrum odit, ut unde non sint, quia sed
-						inventore odio, delectus iure! Eligendi vitae fuga blanditiis
-						accusamus tempore! Lorem ipsum dolor sit amet consectetur
-						adipisicing elit. Nihil natus recusandae amet nostrum odit, ut unde
-						non sint, quia sed inventore odio, delectus iure! Eligendi vitae
-						fuga blanditiis accusamus tempore!
+						I am an aspring Full Stack Developer, currently based in the West
+						Midlands, I have held a great passion for computing and IT my whole
+						life which led to my completing a BSC honours Computer Security
+						degree in 2020. I then decided to further enhance my skills by
+						completing a full stack development certification at the University
+						of Birmingham in 2022, whilst acquiring these qualifications I have
+						been working in a management role within the retail industry which
+						has also further enhanced my skills in multiple areas. The next
+						stage of my life is about pursuing and attaining a full time career
+						in the computing/ IT industry and envisage myself joining and
+						becoming part of a creative and productive team at a forward looking
+						company within the industry.
 					</p>
 				</AnimationOnScroll>
 			</AnimationOnScroll>
